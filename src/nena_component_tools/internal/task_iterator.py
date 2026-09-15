a334ed52429156ea0f8db98c51134e3ea8cbfe61
Greg Olmschenk <greg@olmschenk.com>
2026-09-15 16:14:05 -0400

    Use direct pages building instead of third party action

--- src/nena_component_tools/internal/task_iterator.py
+++ src/nena_component_tools/internal/task_iterator.py
@@ -8,7 +8,8 @@ from typing import Iterator
 import boto3
 
 from nena_component_tools.internal.environment_constants import DEPLOYMENT_TYPE, DeploymentType
-from nena_component_tools.internal.task import Task, EventPipelineTaskMetadata, EventPipelineTask
+from nena_component_tools.internal.event_pipeline_task import EventPipelineTask
+from nena_component_tools.internal.task import Task, EventPipelineTaskMetadata
 from nena_component_tools.internal.local_task import LocalTaskMetadata, LocalTask
 
 logger = logging.getLogger()
